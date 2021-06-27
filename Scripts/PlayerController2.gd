df9bc5cf222513e2aecaed734972bb56832a2b9e
Edoardo Scibilia <edoardoscibilia@gmail.com>
2021-06-27 23:04:31 +0200

    COYOTE TIME

--- Scripts/PlayerController2.gd
+++ Scripts/PlayerController2.gd
@@ -7,15 +7,16 @@ const PLAYER_AIR_FRICTION = 10
 const GRAVITY = 1000
 const JUMP_SPEED = -300
 const AIR_CONTROL_MULTI = 0.2
+const COYOTE_TIME = 0.1
 
-
+var can_coyote_jump = true
 var velocity = Vector2.ZERO
 var gravity_Vector = Vector2(0,GRAVITY)
 var horizontal_input
 var is_jumping = false
 var jump_input
 
-onready var GC_area = get_node("Ground_check_area")
+onready var GroundCheck_area = get_node("Ground_check_area")
 onready var player_sprite = get_node("Sprite")
 
 func _physics_process(delta):
@@ -26,13 +27,16 @@ func _physics_process(delta):
 	
 	#UPDATE GROUNDCHECK
 	var is_on_ground = im_on_ground()
-	
+#	and is_on_ground 
 	#JUMPING
-	if jump_input and is_on_ground :
-		is_jumping = true
-		velocity.y = JUMP_SPEED
+	if jump_input :
+		if can_coyote_jump or is_on_ground:
+			is_jumping = true
+			velocity.y = JUMP_SPEED
+			can_coyote_jump = false
 	elif is_jumping and is_on_ground :
 		is_jumping = false
+
 	#AIR CONTROL
 	if is_jumping :
 		horizontal_input *= AIR_CONTROL_MULTI
@@ -49,14 +53,18 @@ func _physics_process(delta):
 		if horizontal_input != 0 :
 			velocity = velocity.move_toward(Vector2(0, velocity.y), PLAYER_AIR_FRICTION / 2)
 		else : velocity = velocity.move_toward(Vector2(0, velocity.y), PLAYER_AIR_FRICTION)
-	
+
 	#MOVEMENT RESULT
 	if !is_on_ground :
 		velocity.y += GRAVITY * delta
-	
+		if can_coyote_jump == true :
+			stopcoyoteTime()
+
+
+	if is_on_ground :
+		can_coyote_jump = true
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
-	
 	
 func _process(_delta):
 	
@@ -71,11 +79,10 @@ func _process(_delta):
 
 #Get GROUNDCHECK
 func im_on_ground() -> bool :
-	var on_ground = GC_area.player_on_ground
+	var on_ground = GroundCheck_area.player_on_ground
 	return on_ground
 	
-#func coyoteTime():
-#	yield(get_tree().create_timer(COYOTE_TIME), "timeout")
-#	can_coyote_jump = false
-#	print (can_coyote_jump)
-#	pass
+func stopcoyoteTime():
+	yield(get_tree().create_timer(COYOTE_TIME), "timeout")
+	can_coyote_jump = false
+	pass
