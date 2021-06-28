512312b400e4e10feee73251b2a9ce2fd1f02a29
Edoardo Scibilia <edoardoscibilia@gmail.com>
2021-06-28 20:04:25 +0200

    EUREKA SLOWING WHILE JUMPING
    Now MAX_SPEED gets updated when jumping (clamped between -200 and 200) and gets smaller linearly during the jump

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -1,14 +1,16 @@
 extends KinematicBody2D
 
 const ACCELERATION = 50 #was 35
-const MAX_SPEED = 280
+const DEFAULT_MAX_SPEED = 280
 const PLAYER_FRICTION = 50 #was 25
 const PLAYER_AIR_FRICTION = 10
+const FRICTION_FOR_MAX_SPEED = .2
 const GRAVITY = 1000
 const JUMP_SPEED = -300
-const AIR_CONTROL_MULTI = 0.2
+const AIR_CONTROL_MULTI = 0.3
 const COYOTE_TIME = 0.1
 
+var max_speed = 280
 var can_coyote_jump = true
 var velocity = Vector2.ZERO
 var gravity_Vector = Vector2(0,GRAVITY)
@@ -27,30 +29,31 @@ func _physics_process(delta):
 	
 	#UPDATE GROUNDCHECK
 	var is_on_ground = im_on_ground()
-
+	
+	#AIR CONTROL
+	if is_jumping :
+		horizontal_input *= AIR_CONTROL_MULTI
+		max_speed -= FRICTION_FOR_MAX_SPEED
+		
 	#JUMPING
 	if jump_input :
 		if can_coyote_jump or is_on_ground:
 			is_jumping = true
 			velocity.y = JUMP_SPEED
+			max_speed = update_jumping_max_speed()
 			can_coyote_jump = false
 	elif is_jumping and is_on_ground :
 		is_jumping = false
 
-	#AIR CONTROL
-	if is_jumping :
-		horizontal_input *= AIR_CONTROL_MULTI
-	
 	#MOVEMENT
 	if horizontal_input != 0 :
 		velocity.x += horizontal_input * ACCELERATION
-		velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
-
+		velocity.x = clamp(velocity.x, -abs(max_speed), max_speed)
 	#APPLY FRICTION
 	elif !is_jumping :
 		velocity = velocity.move_toward(Vector2(0, velocity.y), PLAYER_FRICTION)
 	else :
-		velocity = velocity.move_toward(Vector2(0, velocity.y), calcAirFriction())
+		velocity = velocity.move_toward(Vector2(0, velocity.y), PLAYER_AIR_FRICTION)
 
 	#MOVEMENT RESULT
 	if !is_on_ground :
@@ -58,9 +61,9 @@ func _physics_process(delta):
 		if can_coyote_jump == true :
 			stopcoyoteTime()
 
-
 	if is_on_ground :
 		can_coyote_jump = true
+		max_speed = DEFAULT_MAX_SPEED
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
@@ -75,6 +78,9 @@ func _process(_delta):
 	#LOCK X IN VIEWPORT
 	transform.origin.x = clamp(transform.origin.x, Global.left_playground_wall_pos + 8, Global.right_playground_wall_pos - 8)
 
+func update_jumping_max_speed() -> float :
+	return clamp(velocity.x, -200, 200)
+
 #Get GROUNDCHECK
 func im_on_ground() -> bool :
 	var on_ground = GroundCheck_area.player_on_ground
@@ -85,10 +91,10 @@ func stopcoyoteTime():
 	can_coyote_jump = false
 	pass
 
-func calcAirFriction():
-	if horizontal_input == 0 :
-		return PLAYER_AIR_FRICTION
-	elif horizontal_input * velocity.x <= 0 :
-		return PLAYER_AIR_FRICTION
-	else :
-		return PLAYER_AIR_FRICTION * 0.5
+#func calcAirFriction():
+#	if horizontal_input == 0 :
+#		return PLAYER_AIR_FRICTION
+#	elif horizontal_input * velocity.x <= 0 :
+#		return PLAYER_AIR_FRICTION
+#	else :
+#		return PLAYER_AIR_FRICTION * 0.5
--- Scripts/Player_label.gd
+++ Scripts/Player_label.gd
@@ -3,4 +3,4 @@ extends Label
 onready var player_node = Global.Player_node
 
 func _process(delta):
-	text = str(player_node.velocity.x)
+	text = str(player_node.max_speed)
